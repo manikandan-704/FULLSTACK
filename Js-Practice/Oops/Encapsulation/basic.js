52abd38bbf04fd985d6f30de5df85110e95033c5
manikandan-704 <nagarajpriyan2004@gmail.com>
2026-01-24 22:08:34 +0530

    Oops-Encapsulation

---
+++ Js-Practice/Oops/Encapsulation/basic.js
@@ -0,0 +1,38 @@
+class Encapsulation {
+    #mark;  //private variable
+    setMark(mark) {
+        this.#mark = mark;
+    }
+    getMark() {
+        return this.#mark;
+    }
+}
+const s1 = new Encapsulation();
+s1.setMark(85);
+console.log(s1.getMark());
+
+
+class Account {
+    #bal = 0;
+    setDeposit(amt) {
+        if (amt < 1000 || amt === 0) {
+            return console.log("Minimum balance should be 1000");
+        }
+        if (amt === "" || typeof amt !== "number") {
+            return console.log("Enter the deposit amount");
+        }
+        this.#bal += amt;
+    }
+    getBal() {
+        return this.#bal;
+    }
+}
+const user = new Account();
+user.setDeposit(0);
+user.setDeposit(1000);
+user.setDeposit("abc");
+user.setDeposit(1000);
+user.setDeposit(1000);
+user.setDeposit(1000);
+user.setDeposit(0);
+console.log(`BALANCE: ${user.getBal()}`);
