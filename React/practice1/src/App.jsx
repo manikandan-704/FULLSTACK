d6ae894ccb5e51f9d0ef8525ee03a822af006ba3
manikandan-704 <nagarajpriyan2004@gmail.com>
2026-01-31 21:20:41 +0530

    React changes

---
+++ React/practice1/src/App.jsx
@@ -0,0 +1,13 @@
+import  './App.css'
+import List from './components/List.jsx'
+import Search from './components/Search'
+function App() {
+  return (
+    <>
+    <List/>
+    <Search/>
+    </>
+  )
+}
+
+export default App
