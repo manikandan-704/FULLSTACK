0989a07e3a1654887f8b04eca36f2d5df89e0989
manikandan-704 <nagarajpriyan2004@gmail.com>
2026-02-11 19:24:40 +0530

    modified

--- DSA/Array/reverse.js
+++ DSA/Array/reverse.js
@@ -1,11 +1,17 @@
-let arr=[1,2,3,4,5,6,7] ;            //two pointer methode reversing an array
+let arr=[1,2,3,4,5,6,7] ;  
+          
 let left=0;
+
 let right=arr.length-1;
+
 while(left<right){
+
     let temp=arr[left];
     arr[left]=arr[right];
     arr[right]=temp;
+    
     left++
+
     right--
 }
 console.log(arr);
--- DSA/interview/long-word.js
+++ DSA/interview/long-word.js
@@ -1,12 +1,23 @@
+
 function longestWord(str) {
+ 
   let words = str.split(" "); //splits the sentence ["Finding", "the", "longestWord", "Sentence!!!!!"]
+  
   let longest = "";
+  
   for (let word of words) {
+   
     //loop through the words
     if (word.length > longest.length) {
+     
       longest = word;
+
     }
   }
+  
   return longest;
+
 }
-console.log(longestWord("Finding the longestWord in the Sentence!!!!!"));
+
+let result=longestWord("Finding the longestWord in the Sentence!!!!!");
+console.log(result);
