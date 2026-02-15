2bad4b9f3c944a58124b41ea57bc97f70d40524c
manikandan-704 <nagarajpriyan2004@gmail.com>
2026-02-15 19:50:11 +0530

    recalled fetch API and async/await

--- Js-Practice/Oops/Encapsulation/basic.js
+++ Js-Practice/Oops/Encapsulation/basic.js
@@ -1,5 +1,5 @@
 class Encapsulation {
-    #mark;  //private variable
+    #mark;                 //private variable
     setMark(mark) {
         this.#mark = mark;
     }
@@ -16,12 +16,13 @@ class Account {
     #bal = 0;
     setDeposit(amt) {
         if (amt < 1000 || amt === 0) {
-            return console.log("Minimum balance should be 1000");
+            return console.log("Minimum deposite should be $1000");
         }
         if (amt === "" || typeof amt !== "number") {
             return console.log("Enter the deposit amount");
         }
         this.#bal += amt;
+        console.log(`Amount is Deposited $${amt}`);
     }
     getBal() {
         return this.#bal;
@@ -31,8 +32,8 @@ const user = new Account();
 user.setDeposit(0);
 user.setDeposit(1000);
 user.setDeposit("abc");
-user.setDeposit(1000);
-user.setDeposit(1000);
-user.setDeposit(1000);
+user.setDeposit(1050);
+user.setDeposit(1500);
+user.setDeposit(3500);
 user.setDeposit(0);
 console.log(`BALANCE: ${user.getBal()}`);
